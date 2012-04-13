f730559530ade80892d7d1a029ec2ca96a7a2c64
Mark Rada <mrada@marketcircle.com>
2012-04-13 12:28:50 -0400

    Add some basic tests

---
+++ MouseTest.c
@@ -0,0 +1,26 @@
+
+#include <ApplicationServices/ApplicationServices.h>
+#include <assert.h>
+#include "Mouse.h"
+
+static void
+run_test(CGPoint point) {
+  mouse_move(point);
+  assert(mouse_position().x == point.x);
+  assert(mouse_position().y == point.y);
+}
+
+int
+main() {
+
+  run_test(CGPointMake(100, 100));
+  run_test(CGPointMake(900, 100));
+  run_test(CGPointMake(900, 100));
+  run_test(CGPointMake(900, 900));
+  run_test(CGPointMake(100, 900));
+  run_test(CGPointMake(100, 100));
+  run_test(CGPointMake(900, 900));
+
+  return 0;
+}
+
