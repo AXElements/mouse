79dbe7006ac4eb64c2bfe4e000f63048a07870d9
Mark Rada <marada@uwaterloo.ca>
2012-12-02 17:11:42 -0500

    Document stuff in lib/mouse.rb

--- lib/mouse.rb
+++ lib/mouse.rb
@@ -4,21 +4,42 @@ if RUBY_ENGINE == 'macruby'
 
 else
 
+  ##
+  #
   class CGPoint < Struct.new(:x, :y)
-
-    def to_point
-      self
-    end
-
+    ##
+    # Return a nice string representation of the point
+    #
+    # Overrides `Object#inspect` to more closely mimic MacRuby `Boxed#inspect`.
+    #
+    # @return [String]
     def inspect
       "#<CGPoint x=#{self[:x]} y=#{self[:y]}>"
     end
-
   end
 
 end
 
+
+##
+# Mouse extensions to `CGPoint`
+class CGPoint
+  ##
+  # Returns the receiver, since the receiver is already a {CGPoint}
+  #
+  # @return [CGPoint]
+  def to_point
+    self
+  end
+end
+
+##
+# Mouse extensions to `Array`
 class Array
+  ##
+  # Coerce the first two elements of the receiver into a {CGPoint}
+  #
+  # @return [CGPoint]
   def to_point
     CGPoint.new self[0], self[1]
   end
