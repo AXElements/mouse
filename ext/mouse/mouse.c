fc7fade372b45cebb38eea2dd06604cb96801cc6
Mark Rada <marada@uwaterloo.ca>
2012-12-02 03:33:51 -0500

    Add Mouse.move_to

--- ext/mouse/mouse.c
+++ ext/mouse/mouse.c
@@ -3,6 +3,8 @@
 
 static VALUE rb_mMouse;
 static VALUE rb_cCGPoint;
+static ID sel_x;
+static ID sel_y;
 
 static
 VALUE
@@ -12,9 +14,26 @@ rb_mouse_wrap_point(CGPoint point)
 }
 
 static
+CGPoint
+rb_mouse_unwrap_point(VALUE point)
+{
+  double x = NUM2DBL(rb_struct_getmember(point, sel_x));
+  double y = NUM2DBL(rb_struct_getmember(point, sel_y));
+  return CGPointMake(x, y);
+}
+
+static
 VALUE
 rb_mouse_current_position(VALUE self)
 {
+  return rb_mouse_wrap_point(mouse_current_position());
+}
+
+static
+VALUE
+rb_mouse_move_to(VALUE self, VALUE point)
+{
+  mouse_move_to(rb_mouse_unwrap_point(point));
   return rb_mouse_wrap_point(mouse_current_position());
 }
 
@@ -22,9 +41,12 @@ void
 Init_mouse()
 {
   rb_cCGPoint = rb_const_get(rb_cObject, rb_intern("CGPoint"));
+  sel_x = rb_intern("x");
+  sel_y = rb_intern("y");
 
   rb_mMouse = rb_define_module("Mouse");
   rb_funcall(rb_mMouse, rb_intern("extend"), 1, rb_mMouse);
 
   rb_define_method(rb_mMouse, "current_position", rb_mouse_current_position, 0);
+  rb_define_method(rb_mMouse, "move_to",          rb_mouse_move_to,          1);
 }
