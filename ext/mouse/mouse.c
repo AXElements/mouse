e5f8df38dfd963ae334334c007097922eea6260b
Mark Rada <marada@uwaterloo.ca>
2012-12-02 16:12:06 -0500

    Add Mouse.drag_to and Mouse.scroll

--- ext/mouse/mouse.c
+++ ext/mouse/mouse.c
@@ -5,6 +5,10 @@ static VALUE rb_mMouse;
 static VALUE rb_cCGPoint;
 static ID sel_x;
 static ID sel_y;
+static ID sel_to_point;
+static ID sel_to_i;
+
+#define CURRENT_POSITION rb_mouse_wrap_point(mouse_current_position())
 
 static
 VALUE
@@ -35,6 +39,33 @@ rb_mouse_move_to(VALUE self, VALUE point)
 {
   mouse_move_to(rb_mouse_unwrap_point(point));
   return rb_mouse_wrap_point(mouse_current_position());
+/***
+ * Drag the cursor to the given co-ordinates
+ *
+ * @param point [CGPoint,Array(Number,Number),#to_point]
+ * @return [CGPoint]
+ */
+static
+VALUE
+rb_mouse_drag_to(VALUE self, VALUE point)
+{
+  mouse_drag_to(rb_mouse_unwrap_point(point));
+  return CURRENT_POSITION;
+}
+
+/***
+ * Generate `amount` scroll events at the current cursor position
+ *
+ * @param amount [Number]
+ * @return [CGPoint]
+ */
+static
+VALUE
+rb_mouse_scroll(VALUE self, VALUE amount)
+{
+  amount = rb_funcall(amount, sel_to_i, 0);
+  mouse_scroll(NUM2SIZET(amount));
+  return CURRENT_POSITION;
 }
 
 void
@@ -43,10 +74,14 @@ Init_mouse()
   rb_cCGPoint = rb_const_get(rb_cObject, rb_intern("CGPoint"));
   sel_x = rb_intern("x");
   sel_y = rb_intern("y");
+  sel_to_point = rb_intern("to_point");
+  sel_to_i     = rb_intern("to_i");
 
   rb_mMouse = rb_define_module("Mouse");
   rb_funcall(rb_mMouse, rb_intern("extend"), 1, rb_mMouse);
 
   rb_define_method(rb_mMouse, "current_position", rb_mouse_current_position, 0);
   rb_define_method(rb_mMouse, "move_to",          rb_mouse_move_to,          1);
+  rb_define_method(rb_mMouse, "drag_to",          rb_mouse_drag_to,          1);
+  rb_define_method(rb_mMouse, "scroll",           rb_mouse_scroll,           1);
 }
